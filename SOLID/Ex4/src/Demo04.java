570d65b203bf6769358cb4bff95dae22372ffecc
Vivaan Goyal <samgoyal.2006@gmail.com>
2026-02-23 04:55:19 +0530

    changed the name of the main file of Ex4 to Demo04

--- SOLID/Ex4/src/Demo04.java
+++ SOLID/Ex4/src/Demo04.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class Demo04 {
     public static void main(String[] args) {
         System.out.println("=== Hostel Fee Calculator ===");
         BookingRequest req = new BookingRequest(LegacyRoomTypes.Double, List.of(AddOn.laundry, AddOn.mess));
